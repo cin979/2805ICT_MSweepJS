8b596ab87296ad34464c3184e4e97bf9bbd3130f
Cin <cin979@gmail.com>
2019-08-02 15:54:23 +1000

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { ScoresComponent } from './scores/scores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    ScoresComponent
   ],
   imports: [
     BrowserModule,
