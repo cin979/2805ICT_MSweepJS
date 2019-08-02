217cce5c30a9648bcb92ff270146628a30cc0908
Cin <cin979@gmail.com>
2019-08-02 15:59:31 +1000

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameComponent } from './game/game.component';
+import { ScoresComponent } from './scores/scores.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'game', component:GameComponent},
+  {path:'scores', component:ScoresComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
